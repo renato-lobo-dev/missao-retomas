8e04f8ed8869a83a8e102c2a3d3d83a2cd1e7d40
renato-lobo-dev <renato.lobo991@gmail.com>
2026-05-05 00:19:13 +0100

    Update questions and answers in perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -7,11 +7,13 @@ export const perguntas = [
     opcoes: {
       A: "Aceitas a retoma normalmente",
       B: "Explicas que o modelo já não é elegível",
-      C: "Aceitas mas com valor simbólico"
+      C: "Aceitas mas com valor simbólico",
+      D: "Informas que infelizmente já não retomamos esse equipamento, mas que estamos disponiveis para analisar outros equipamentos no futuro"
     },
-    correta: "B",
+    correta: "D",
+    explicacao: "O iPhone 11 já não é elegível para retoma. É fundamental explicar o motivo,redirecionar a conversa para uma alternativa de venda e mostrar disponbilidade ao cliente.",
     impacto: {
-      correta: { pn: +10, pc: -5 },
+      correta: { pn: +10, pc: 0 },
       errada:  { pn: -15, pc: -10 }
     }
   },
@@ -22,12 +24,13 @@ export const perguntas = [
     pergunta: "O cliente diz que lhe foi dado outro valor de retoma na semana passada. Como respondes?",
     opcoes: {
       A: "Aceitas o valor antigo",
-      B: "Explicas a volatilidade e validade diária",
+      B: "Explicas a volatilidade e validade diária, mostrando disponbilidade para fazer a retoma na mesma",
       C: "Recusas sem justificar"
     },
     correta: "B",
+    explicacao: "É importante informar o cliente do motivo da não aceitação do equipamento do valor antigo, mostrando-nos sempre disponiveis para a conclusão do processo pelo valor no sistema hoje. Que pode não ser o mesmo amanhã. ",
     impacto: {
-      correta: { pn: +10, pc: +10 },
+      correta: { pn: +10, pc: +5 },
       errada:  { pn: -15, pc: -15 }
     }
   },
@@ -35,16 +38,17 @@ export const perguntas = [
   {
     id: 3,
     tipo: "tecnico",
-    pergunta: "Um iPhone com riscos visíveis no ecrã, mas funcional, deve ser classificado como:",
+    pergunta: "Um iPhone com uma pequena mossa no canto do equipamento,tampa traseira partida,mas completamente funcional, deve ser classificado como:",
     opcoes: {
-      A: "Grade A",
+      A: "Grade D",
       B: "Grade B",
       C: "Grade C"
     },
-    correta: "C",
+    correta: "A",
+    explicacao: "Um equipamento com algo dano físico é sempre um GRADE D, apenas recusamos equipamentos com humidade e com o corpo dobrado.",
     impacto: {
       correta: { pn: +10, pc: +5 },
-      errada:  { pn: -10, pc: -5 }
+      errada:  { pn: -10, pc: -10 }
     }
   },
 
@@ -55,9 +59,10 @@ export const perguntas = [
     opcoes: {
       A: "Sim, desde que funcione",
       B: "Sim, mas com valor reduzido",
-      C: "Não pode ser aceite"
+      C: "Não pode ser aceite e explicamos o motivo da impossibilidade"
     },
     correta: "C",
+    explicacao: "A marcação CE é extremamente importante e não pode ser ignorada. Devemos sempre ter o cuidado de explicar ao cliente o motivo para não o afastar de uma possivel retoma no futuro...com a marcação CE.",
     impacto: {
       correta: { pn: +10, pc: 0 },
       errada:  { pn: -20, pc: -5 }
@@ -74,6 +79,7 @@ export const perguntas = [
       C: "Encerrar rapidamente a conversa"
     },
     correta: "B",
+    explicacao: "Mostrar ao cliente que compreendemos, mas que é o mercado da oferta e da procura, e que no que vai ter um retorno financeiro pelo equipamento o que vai aliviar o custo da nova aquisição.",
     impacto: {
       correta: { pn: +5, pc: +15 },
       errada:  { pn: -5, pc: -20 }
@@ -83,13 +89,14 @@ export const perguntas = [
   {
     id: 6,
     tipo: "comercial",
-    pergunta: "Que oportunidade comercial surge com a retoma?",
+    pergunta: "Que oportunidade(s) comercial surge com a retoma?",
     opcoes: {
       A: "Apenas venda do novo equipamento",
       B: "Upgrade + serviços adicionais",
       C: "Nenhuma vantagem adicional"
     },
     correta: "B",
+    explicacao: "A retoma do equipamento do cliente vai permitir ao cliente fazer um upgrade à compra que vai fazer bem como adquirir serviços adicionais como Plano de Proteção/Capa/Pelicula/Carregador...etc",
     impacto: {
       correta: { pn: +15, pc: +5 },
       errada:  { pn: -5, pc: -5 }
@@ -102,10 +109,11 @@ export const perguntas = [
     pergunta: "Qual é o verdadeiro objetivo do serviço de retomas?",
     opcoes: {
       A: "Aumentar stock usado",
-      B: "Aumentar vendas e fidelização",
-      C: "Eliminar equipamentos antigos"
+      B: "Aumentar vendas, fidelizar os cliente e dar uma segunda vida aos equipamentos dos mesmos",
+      C: "Nenhum"
     },
     correta: "B",
+    explicacao: "Ao colocar dinheiro no bolso do cliente estamos a criar potenciais clientes, a fidelizá-los para a retoma connosco no próximo lançamento e ao mesmo tempo alimentar dar uma segunda vida aos seus equipamentos para venda nas nossas lojas.",
     impacto: {
       correta: { pn: +10, pc: +10 },
       errada:  { pn: 0, pc: -10 }
